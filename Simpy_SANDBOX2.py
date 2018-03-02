4fb6428cfb2089f4e3dd60fd2d9651ac286aca47
Vicente Herrero (zeus) <viherbos@eln.upv.es>
2018-03-02 10:18:15 +0100

    ubuntu

--- Simpy_SANDBOX2.py
+++ Simpy_SANDBOX2.py
@@ -2,45 +2,70 @@ import simpy
 import random
 from simpy.events import AnyOf, AllOf, Event
 
-lost_events_counter = 0
+
 
 class FullError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        lost_events_counter++
         return repr(self.value)
 
-def print_stats(env,res):
-    #print('%d of %d slots are allocated.' % (res.count, res.capacity))
-    print ('TIME: %d // %d of %d slots are allocated.' % (env.now,res.count, res.capacity))
-    #print('  Users:', res.users)
-    #print('  Queued events:', res.queue)
 
-def consumer(env, resource):
-    yield env.timeout(random.randint(1,10))
-    
-    print_stats(env,resource)
 
-    try:
-        if resource.count == 2:
-             raise FullError('Lost event')
-        with resource.request() as req:
-            #yield req
-            #print_stats(env,resource)
-            yield env.timeout(random.randint(1,5))
-            # Automatic release of the resource
-    except FullError:
-        print ("Lost Event at %d" % env.now)
+
+
+class producer(object):
+
+    def __init__(self,env):
+        self.env = env
+        self.out = None
+        # Connection with receptor
+        self.action = env.process(self.run())
+
+    def run(self):
+        while self.env.now < 100:
+            yield self.env.timeout(random.randint(1,10))
+            #print_stats(env,self.out.res)
+            self.out.put()
+
+
+class consumer(object):
+
+    def __init__(self,env,lost_ev):
+        self.env = env
+        self.res = simpy.Resource(self.env,capacity=2)
+        self.lost = lost_ev
+
+    def print_stats(self):
+        print ('TIME: %d // %d of %d slots are allocated.' % (self.env.now,\
+                                                self.res.count, self.res.capacity))
+
+    def put(self):
+        #self.print_stats(self.env,self.res)
+        print ("HOLA")
+        with self.res.request() as req:
+             # Automatic release of the resource
+             yield self.env.timeout(random.randint(1,5))
+
+        # try:
+        #     print_stats(env,self.res)
+        #     if self.res.count == 2:
+        #         raise FullError('Lost event')
+        #     with self.res.request() as req:
+        #         # Automatic release of the resource
+        #         yield self.env.timeout(random.randint(1,5))
+        # except FullError:
+        #     print ("Lost Event at %d" % self.env.now)
+        #     self.lost += 1
 
 
 if __name__ == '__main__':
 
     env = simpy.Environment()
-    cola = simpy.Resource(env,capacity=2)
-    consumers = [env.process(consumer(env,cola)) for i in range(5)]
+    lost_counter = 0
+    C = consumer(env,lost_counter)
+    P = producer(env)
+    P.out = C
+
     env.run(until=100)
-    print ("Total Lost Events %d" % lost_events_counter)
-    # for i in range(1,100):
-    #     env.run(until=i)
-    #     print_stats(env,cola)
+    print ("Total Lost Events %d" % lost_counter)
